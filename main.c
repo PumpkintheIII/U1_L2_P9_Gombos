37b7c443ef2bedacd265c67e642ec7ed8a696d64
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-02-07 14:24:44 +0000

    Iteration 2

--- main.c
+++ main.c
@@ -13,6 +13,8 @@ int main(void) { //define main function
   adultPrice = 10.5; //set to price of adult ticket
   childPrice = 8; //set to price of child ticket
 
+  printf("This program allows you to enter the number of tickets you would like to buy and it will tell you the total cost. The price of tickets are:\n\t- Child: $8.00\n\t- Adult: $10.50\nThe program will give you two prompts to enter tickets. Please enter the number of tickets you want in whole numbers:\n\t- Child Tickets: Number of Child Tickets\n\t- Adult Tickets: Number of Adult Tickets\n"); //instructions
+  
   printf("Child Tickets: "); //ask for number of child tickets
   scanf("%d", &childNum); //accepts input and formats as integer
   printf("Adult Tickets: "); //ask for number of adult tickets
