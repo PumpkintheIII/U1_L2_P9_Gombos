9eedcdc252b7d3354944c556e312e12b4b676202
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-02-07 14:07:11 +0000

    Iteration 1

--- .ccls-cache/@home@runner@U1L2P9Gombos/main.c
+++ .ccls-cache/@home@runner@U1L2P9Gombos/main.c
@@ -3,9 +3,25 @@
   Ticket Calculator
   Max Gombos
 */
-#include <stdio.h>
+#include <stdio.h> //import input and output functions
 
-int main(void) {
+int main(void) { //define main function
+  int adultNum, childNum; //initialize variable as integer
+  float adultPrice, childPrice; //initialize variable as float
+  float totalCost; //initialize variable as float
+
+  adultPrice = 10.5; //set to price of adult ticket
+  childPrice = 8; //set to price of child ticket
+
+  printf("Child Tickets: "); //ask for number of child tickets
+  scanf("%d", &childNum); //accepts input and formats as integer
+  printf("Adult Tickets: "); //ask for number of adult tickets
+  scanf("%d", &adultNum); //accepts input and formats as integer
+
+  totalCost = childNum * childPrice; //find cost of child tickets
+  totalCost = totalCost + (adultNum * adultPrice); //cost of adult
+
+  printf("Total cost: $%0.2f", totalCost); //print total cost
   
   return 0;
 }
